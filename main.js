6704790493af9e9cd9f602cb00eafeb6ddd9f7c3
Abdou <abdoullaha442@gmail.com>
2022-05-01 11:00:02 +0200

    Data Types

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ window.onload = function () {
 }
 
 console.log('%cLog Vog  %clol viv', 'color: red; font-size: 40px','color: blue; font-size: 40px')
+
+/*
+ --Data Types intro 
+ - String
+ - Number
+ */
+
+ console.log(typeof 'Abdoullah Sidi Aly')
+ console.log(typeof 'Abdoullah Sidi Aly')
+ console.log(typeof 5000)
+ console.log(typeof true)
+ console.log(typeof [10,11,12])
+ console.log(typeof null)
+ console.log(typeof undefined)
+ console.log(typeof [10.2])
+ console.log(typeof {name : "Abdou", age : 17, country : 'RIM'})
